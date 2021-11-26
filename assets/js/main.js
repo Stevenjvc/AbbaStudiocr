6c3cad7fa6874ba58f78394ba71ace8f9576000f
Stevenjvc <steven.jenkinsc@gmail.com>
2021-11-25 19:36:36 -0600

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,110 @@
+/*===== SHOW MENU =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== CHANGE BACKGROUND HEADER =====*/ 
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*===== SHOW SCROLL TOP =====*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scrolltop');
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+/*===== MIXITUP FILTER PORTFOLIO =====*/ 
+const mixer = mixitup('.portfolio-container', {
+    selectors: {
+        target: '.portfolio-content'
+    },
+    animation: {
+        duration: 400
+    }
+});
+/* Link active portfolio */ 
+const linkPortfolio = document.querySelectorAll('.portfolio-item')
+
+function activePortfolio(){
+    if(linkPortfolio){
+        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
+        this.classList.add('active-portfolio')
+    }
+}
+linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
+
+/*===== GSAP ANIMATION =====*/ 
+
+
+/*===== Contact Form =====*/ 
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+form.onsubmit = (e)=>{
+  e.preventDefault();
+  statusTxt.style.color = "#00ee92";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Enviando mensaje";
+  form.classList.add("disabled");
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "message.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState == 4 && xhr.status == 200){
+      let response = xhr.response;
+      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
+    }
+  }
+  let formData = new FormData(form);
+  xhr.send(formData);
+}
+
+
---
+++ assets/php/message.php
@@ -0,0 +1,24 @@
+<?php
+  $nombre = htmlspecialchars($_POST['nombre']);
+  $email = htmlspecialchars($_POST['email']);
+  $telefono = htmlspecialchars($_POST['telefono']);
+  $proyecto = htmlspecialchars($_POST['proyecto']);
+  $mensaje = htmlspecialchars($_POST['mensaje']);
+  if(!empty($email) && !empty($mensaje)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = "info@abba.studio"; //enter that email address where you want to receive all mensajes
+      $subject = "From: $nombre <$email>";
+      $body = "nombre: $nombre\nEmail: $email\ntelefono: $telefono\nproyecto: $proyecto\n\nmensaje:\n$mensaje\n\nRegards,\n$nombre";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Su mensaje a sido enviado";
+      }else{
+         echo "Erro al enviar su mensaje!";
+      }
+    }else{
+      echo "Ingresa una dirección de email valida";
+    }
+  }else{
+    echo "Debes ingresar el email y mensaje";
+  }
+?>
